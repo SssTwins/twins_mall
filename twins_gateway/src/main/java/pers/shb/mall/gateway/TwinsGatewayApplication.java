54f631787468e462cf491d685455235994383ff2
songhongbao <songhongbao@uino.com>
2021-05-22 21:51:23 +0800

    Gateway project configuration update

--- twins_gateway/src/main/java/pers/shb/mall/gateway/TwinsGatewayApplication.java
+++ twins_gateway/src/main/java/pers/shb/mall/gateway/TwinsGatewayApplication.java
@@ -2,8 +2,11 @@ package pers.shb.mall.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class TwinsGatewayApplication {
 
     public static void main(String[] args) {
